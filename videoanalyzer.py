554cecfd103a471940edb3b50e56149ca99708f6
Oscar Nord <oscar.nord@gmail.com>
2018-10-10 16:21:55 +0200

    added frame step and did some cleanup

--- videoanalyzer.py
+++ videoanalyzer.py
@@ -1,7 +1,7 @@
 import argparse
 import cv2
 
-print("OpenCV version",cv2.__version__)
+print("OpenCV version"+cv2.__version__)
 
 def variance_of_laplacian(image):
 	# compute the Laplacian of the image and return the focus measure
@@ -14,13 +14,16 @@ ap.add_argument("-t", "--threshold", type=float, default=100.0,
 args = vars(ap.parse_args())
 
 path = raw_input("Enter path to video file: ")
+step = int(raw_input("frame step: "))
 
+print(step)
 vidcap = cv2.VideoCapture(path)
 
 success, image = vidcap.read()
 count = 0
 blurryFrame = 0
 savedFrame = 0
+frameStep = 0
 success = True
 
 print("Working...")
@@ -28,16 +31,20 @@ print("Working...")
 while success:
   gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
   fm = variance_of_laplacian(gray)
-  if fm >  args["threshold"]:
-    savedFrame += 1
-    cv2.imwrite("frame%d.png" % count, image)
+  print(step, frameStep)
+  if frameStep == step:
+    if fm >  args["threshold"]:
+      savedFrame += 1
+      cv2.imwrite("frame%d.png" % count, image)
+    frameStep = 0
     
   if fm < args["threshold"]:
     blurryFrame += 1
-
+    
   success,image = vidcap.read()
   count += 1
+  frameStep += 1
 
-print("Done!", "Number of frames: ", count)
-print("Saved frames: ", savedFrame)
-print("Discarded frames: ", blurryFrame)
+print("Done!", "Number of frames: "+ str(count))
+print("Saved frames: "+str(savedFrame))
+print("Discarded frames: "+str(blurryFrame))
